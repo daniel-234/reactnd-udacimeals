392e00827b9a402faf15c55fde688c5df07a45dd
Daniele <daniele.rb6@gmail.com>
2017-08-08 11:33:12 +0200

    refactor: Using the Provider

--- src/index.js
+++ src/index.js
@@ -5,11 +5,27 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux'
 import reducer from './reducers'
+import { Provider } from 'react-redux'
 
 const store = createStore(
 	reducer,
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+// We can wrap our main root component inside of Provider
+// and then, instead of passing store to App, we want to
+// pass it to Provider.
+ReactDOM.render(
+	<Provider store={store}>
+	{
+		/* Now whenever any of the components that App renders
+		   (or the App itself) needs access to the Redux store
+		   or needs to dispatch an action, it will be able to
+		   do that more easily.
+	   */
+	}
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
 registerServiceWorker();
