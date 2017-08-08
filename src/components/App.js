22aae3ae6962eb3bb38eb31c2ad7171bc1cfb335
Daniele <daniele.rb6@gmail.com>
2017-08-08 17:28:58 +0200

    refactor: Using connect()

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
+    console.log(this.props)
     return (
       <div>
         Hello World
@@ -10,4 +12,24 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps(calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? calendar[day][meal]
+          : null
+
+        return meals
+      }, {})
+    })),
+  }
+}
+
+// We are also invoking `connect`, which is going to return
+//  a brand new function, which we are going to pass a Component.
+export default connect(
+  mapStateToProps,
+)(App)
