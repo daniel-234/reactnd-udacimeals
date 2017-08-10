35c258a9f6940a926b00044ccffc3e94a49c5f48
Daniele <daniele.rb6@gmail.com>
2017-08-10 17:57:42 +0200

    feat: add combineReducers()

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux'
+
 import {
 	ADD_RECIPE,
 	REMOVE_FROM_CALENDAR
@@ -83,4 +85,7 @@ function calendar (state = initialCalendarState, action) {
 	}
 }
 
-export default calendar
+export default combineReducers({
+	food,
+	calendar
+})
